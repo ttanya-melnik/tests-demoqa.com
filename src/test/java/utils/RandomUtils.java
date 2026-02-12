42bf14e2d18eaaf3100865f7be6b65ed8bf4d165
Tatiana Melnik <tamara81tanucha@mail.ru>
2026-02-12 13:48:49 +0300

    Применила генерацию данных

---
+++ src/test/java/utils/RandomUtils.java
@@ -0,0 +1,75 @@
+package utils;
+
+import java.security.SecureRandom;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomUtils {
+
+  public static void main(String[] args) {
+    System.out.println(getRandomString(10));
+    System.out.println(getRandomEmail());
+    System.out.println(getRandomAddress());
+    System.out.println(getRandomInt(111, 99999999));
+    System.out.println(getRandomNumber());
+    System.out.println(getRandomGender());
+
+  }
+
+
+
+public static String getRandomString(int len) {
+    String AB = "123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+    AB = "123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+    SecureRandom rnd = new SecureRandom();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < len; i++)
+      sb.append(AB.charAt(rnd.nextInt(AB.length())));
+    return sb.toString();
+ }
+
+
+
+
+public static String getRandomEmail() {
+    return getRandomString(10) + "@qa.guru";
+ }
+
+
+
+public static String getRandomAddress() {
+    return getRandomString(10) + " " + getRandomString(10) + " " + getRandomString(10);
+ }
+
+
+
+
+
+
+public static  int getRandomInt(int min, int max) {
+    return ThreadLocalRandom.current().nextInt(min, max + 1);
+}
+
+// +8 (662) 533 - 21 - 40
+ public static String getRandomNumber() {
+
+    return String.format("+%s (%s) %s - %s - %s", getRandomInt(1, 9), getRandomInt(111, 999),
+        getRandomInt(111, 999), getRandomInt(11, 99), getRandomInt(11, 99));
+ }
+
+
+
+
+
+ public static String getRandomGender(){
+
+    String[] genders = {"Male", "Female", "Other"};
+      return getRandomItemFormArray(genders);
+ }
+
+ public static String getRandomItemFormArray(String[] array) {
+    int index = getRandomInt(0, array.length - 1);
+      return array[index];
+ }
+
+
+}
---
+++ src/test/java/utils/RegistrationWithRandomUtilsTests.java
@@ -0,0 +1,67 @@
+package utils;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static utils.RandomUtils.getRandomAddress;
+import static utils.RandomUtils.getRandomEmail;
+import static utils.RandomUtils.getRandomNumber;
+import static utils.RandomUtils.getRandomString;
+
+import com.codeborne.selenide.Configuration;
+import java.io.File;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationWithRandomUtilsTests {
+
+  @BeforeAll
+  static void beforeAll() {
+    Configuration.browserSize = "1920x1080"; // Делаем окно большим, чтобы ничего не съезжало
+    Configuration.baseUrl = "https://demoqa.com"; // url сайта
+    Configuration.pageLoadStrategy = "eager"; // Тесты запускаются быстрее
+    Configuration.timeout = 5000; // Если элемент не появится за 5 секунд, то тест упадёт
+    Configuration.holdBrowserOpen = true;  // После выполнения теста, браузер не закрывается автоматически
+
+  }
+
+  @Test
+  void fillFormTest() {
+
+    // ГЕНЕРИРУЕМ СЛУЧАЙНЫЕ СТРОКИ
+    String firstName = getRandomString(10);
+    String lastName = getRandomString(10);
+    String userEmail = getRandomEmail();
+    String streetAddress = getRandomAddress();
+    String userNumber = getRandomNumber();
+
+
+
+    open("/automation-practice-form");
+    $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+    $("#firstName").setValue(firstName);
+    $("#lastName").setValue(lastName);
+    $("#userEmail").setValue(userEmail);
+    $("#genterWrapper").$(byText("Female")).click();
+    $("#userNumber").setValue("8900333111");
+    $("#dateOfBirthInput").click();
+    $(".react-datepicker__month-select").selectOption("September");
+    $(".react-datepicker__year-select").selectOption("2004");
+    $(".react-datepicker__day--028").click();
+    $("#subjectsInput").setValue("English").pressEnter();
+    $("#hobbies-checkbox-2").parent().click();
+    $("#uploadPicture").uploadFile(new File("src/DataTest/first.jpeg"));
+    $("#currentAddress").setValue(streetAddress);
+    $("#state input").setValue("NCR").pressEnter();
+    $("#city input").setValue("Delhi").pressEnter();
+    $("#submit").click();
+    $(".modal-content").should(appear);
+    $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+    $(".table-responsive").shouldHave(text(firstName), text(lastName));
+  }
+}
+
+
+
